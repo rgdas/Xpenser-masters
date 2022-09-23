350e2faa4fa2d7c01aab6730622535785294f22e
Radha Gobinda Das <98231260+rgdas@users.noreply.github.com>
2022-09-23 22:00:57 +0530

    Add files via upload

---
+++ src/components/footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+const Footer = () => {
+  return (
+       <div>
+       <footer>
+       🔥 Made by Radha Gobinda Das 🔥
+       </footer>
+       </div>
+  );
+};
+
+
+export default Footer;
